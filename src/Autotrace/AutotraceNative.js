eade0d02164b0ea5847ab18de56e912a4750fef7
Nick Winder <nfxdevelopment@gmail.com>
2020-10-24 08:30:34 -0500

    Move memory alloc into wasm

--- src/Autotrace/AutotraceNative.js
+++ src/Autotrace/AutotraceNative.js
@@ -2,83 +2,71 @@ import loadNativeModule from "../WasmLoader";
 import type {FittingOptions} from "./FittingOptions";
 
 export class AutotraceNative {
-    constructor() {
-        this.nativeModule = null;
-        this.fittingOptions = {};
-    }
+  constructor() {
+    this.nativeModule = null;
+    this.fittingOptions = {};
+  }
 
-    loadModule(): Promise {
-        if (this.nativeModule != null) return Promise.resolve(true);
+  loadModule(): Promise {
+    if (this.nativeModule != null) return Promise.resolve(true);
 
-        return new Promise(resolve => {
-            loadNativeModule()
-                .then(({nativeModule}) => {
-                    this.nativeModule = nativeModule;
-                    resolve();
-                });
+    return new Promise(resolve => {
+      loadNativeModule()
+        .then(({nativeModule}) => {
+          this.nativeModule = nativeModule;
+          resolve();
         });
+    });
+  }
+
+  setFittingOptionsProperty(fittingOptions: FittingOptions) {
+    this.fittingOptions = {...this.fittingOptions, ...fittingOptions};
+  }
+
+  async convertImage(imageImage: ArrayBuffer): boolean {
+    await this.loadModule();
+
+    const byteArray = new Uint8Array(imageImage);
+    let vector = new this.nativeModule.VectorUint8()
+    for (let i = 0; i < byteArray.byteLength; i++) {
+      vector.push_back(byteArray[i])
     }
 
-    setFittingOptionsProperty(fittingOptions: FittingOptions) {
-        this.fittingOptions = {...this.fittingOptions, ...fittingOptions};
+    try {
+      let result = this.nativeModule.autotraceRun(
+        vector,
+        JSON.stringify(this.fittingOptions),
+        JSON.stringify({}),
+        JSON.stringify({})
+      );
+
+      if (!result.success)
+        console.error(result.error)
+
+      return result.success
+    } catch (error) {
+      console.error(error)
+      return false;
     }
+  }
 
-    async convertImage(imageImage: ArrayBuffer): boolean {
-        await this.loadModule();
-
-        const byteArray = new Uint8Array(imageImage);
-        const dataPtr = this.nativeModule._malloc(byteArray.byteLength);
-        const dataHeap = new Uint8Array(this.nativeModule.HEAPU8.buffer, dataPtr, byteArray.byteLength);
-        dataHeap.set(byteArray);
-
-        try {
-            let result = this.nativeModule.autotraceRun(
-                dataHeap.byteOffset,
-                dataHeap.byteLength,
-                JSON.stringify(this.fittingOptions),
-                JSON.stringify({}),
-                JSON.stringify({})
-            );
-
-            if (!result.success)
-                console.error(result.error)
-
-            return result.success
-        } catch (error) {
-            console.error(error)
-            return false;
-        } finally {
-            this.nativeModule._free(dataHeap.byteOffset)
+  retrieveConversion(): Promise<String> {
+    return new Promise(((resolve, reject) => {
+      let outputFileResult = this.nativeModule.getOutputFile();
+      if (outputFileResult.success) {
+        // TODO Pretty inefficient and should be optimized.
+        let outputBuffer = new Uint8Array(outputFileResult.value.size());
+        for (let i = 0; i < outputFileResult.value.size(); i++) {
+          outputBuffer[i] = outputFileResult.value.get(i);
         }
-    }
-
-    retrieveConversion(): Promise<String> {
-        return new Promise(((resolve, reject) => {
-            let fileSizeOutput = this.nativeModule.outputFileSize();
-
-            if (fileSizeOutput.success) {
-                const byteArrayOutput = new Uint8Array(fileSizeOutput.value);
-                const dataPtrOutput = this.nativeModule._malloc(byteArrayOutput.byteLength);
-                const outputFileHeap = new Uint8Array(this.nativeModule.HEAPU8.buffer, dataPtrOutput, byteArrayOutput.byteLength);
-                outputFileHeap.set(byteArrayOutput);
-
-                try {
-                    let outputFileResult = this.nativeModule.getOutputFile(outputFileHeap.byteOffset, outputFileHeap.byteOffset);
-                    if (outputFileResult.success) {
-                        const enc = new TextDecoder("utf-8");
-                        const svg = enc.decode(outputFileHeap);
-                        resolve(svg);
-                    } else {
-                        reject("output file get error = " + outputFileResult.error);
-                    }
-                } finally {
-                    this.nativeModule._free(outputFileHeap.byteOffset)
-                }
-            } else {
-                reject("file Size error = " + fileSizeOutput.error);
-            }
-        }))
-    }
+        const enc = new TextDecoder("utf-8");
+        const svg = enc.decode(outputBuffer);
+        resolve(svg);
+      } else {
+        reject("output file get error = " + outputFileResult.error);
+      }
+    }))
+  }
 }
 
 export let autotraceNative = new AutotraceNative();
